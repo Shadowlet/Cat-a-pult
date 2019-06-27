876b7c7ed75420ada7e2289a60fb0db1d73217c6
Zachary Carreiro <zacharycarreiro@gmail.com>
2019-06-26 20:43:44 -0400

    Block Creation cleaned up. Needs 2nd player cam, and battle phase.

--- Cat-a-pult/Swift Files/GameScene.swift
+++ Cat-a-pult/Swift Files/GameScene.swift
@@ -41,6 +41,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
     
     let label = SKLabelNode()
+    
+    
+    // UI
+    var buttonFinish = SKShapeNode(circleOfRadius: 100)
+    var blockHUD = SKSpriteNode()
     let labelBlock = SKLabelNode()
     
     public var blockNum: Int = 0
@@ -68,7 +73,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
         
 //        setupDragLabel()
-        setupCreationBlocks()
+        //setupCreationBlocks()
+        setupCreationUI()
     }
     
     // We will use this to switch from the menu scene to the game scene
@@ -103,30 +109,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     func setupCreationBlocks() {
         
-        blockOne = SKSpriteNode(imageNamed: "wood_horiz1")
-        blockOne.setScale(0.5)
-        blockOne.position = CGPoint(x: 392, y: 1116)
-        blockOne.physicsBody?.isDynamic = false
-        addChild(blockOne)
         
-        blockTwo = SKSpriteNode(imageNamed: "wood_vert1")
-        blockTwo.setScale(0.5)
-        blockTwo.position = CGPoint(x: 960, y: 1116)
-        blockTwo.physicsBody?.isDynamic = false
-        addChild(blockTwo)
-        
-        blockThree = SKSpriteNode(imageNamed: "rock_L_horizontal")
-        blockThree.setScale(0.5)
-        blockThree.position = CGPoint(x: 1500, y: 1076)
-        blockThree.physicsBody?.isDynamic = false
-        addChild(blockThree)
-        
-        labelBlock.fontName = "Arial"
-        labelBlock.fontSize = 140
-        labelBlock.color = SKColor.black
-        labelBlock.position = CGPoint(x: frame.midX, y: frame.midY)
-        labelBlock.text = "\(blockCount)"
-        addChild(labelBlock)
         
 //        yellowBin.color = SKColor.yellow
 //        yellowBin.size = CGSize(width: 200, height: 200)
@@ -140,6 +123,56 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 //        blueBin.zPosition = 1
 //        addChild(blueBin)
         
+    }
+    
+    func setupCreationUI() {
+        // Playable margin..
+        let maxAspectRatio: CGFloat = 16.0/9.0
+        let maxAspectRatioHeight = size.width / maxAspectRatio
+        let playableMargin: CGFloat = (size.height - maxAspectRatioHeight)/2
+        
+        blockHUD.color = SKColor.white
+        blockHUD.size = CGSize(width: size.width, height: size.height * 0.15)
+        blockHUD.position = CGPoint(x: size.width/2, y: size.height-playableMargin*1.60)
+        blockHUD.zPosition = 1
+        addChild(blockHUD)
+       
+        buttonFinish.position = CGPoint(x: 1850, y: 950)
+        buttonFinish.strokeColor = UIColor.black
+        buttonFinish.glowWidth = 1.0
+        buttonFinish.fillColor = UIColor.red
+        addChild(buttonFinish)
+        
+        blockOne = SKSpriteNode(imageNamed: "wood_horiz1")
+        blockOne.setScale(0.5)
+        blockOne.position = CGPoint(x: 300, y: 1225)
+        blockOne.zPosition = 2
+        blockOne.physicsBody?.isDynamic = false
+        addChild(blockOne)
+        //
+        //
+        blockTwo = SKSpriteNode(imageNamed: "wood_vert1")
+        blockTwo.setScale(0.5)
+        blockTwo.position = CGPoint(x: 600, y: 1225)
+        blockTwo.zPosition = 2
+        blockTwo.physicsBody?.isDynamic = false
+        addChild(blockTwo)
+        //
+        //
+        blockThree = SKSpriteNode(imageNamed: "rock_L_horizontal")
+        blockThree.setScale(0.5)
+        blockThree.position = CGPoint(x: 900, y: 1225)
+        blockThree.zPosition = 2
+        blockThree.physicsBody?.isDynamic = false
+        addChild(blockThree)
+       
+        
+        labelBlock.fontName = "Arial"
+        labelBlock.fontSize = 140
+        labelBlock.color = SKColor.black
+        labelBlock.position = CGPoint(x: frame.midX, y: frame.midY)
+        labelBlock.text = "\(blockCount)"
+        addChild(labelBlock)
     }
     
 //    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -182,16 +215,20 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch = touches.first!
         print(blockSprite.position)
         
         //blockSprite.removeFromParent()
-        if blockCount == maxBlocks {
-            for n in 0...maxBlocks-1 {
+        
+        if buttonFinish.frame.contains(touch.previousLocation(in: self)) {
+            for n in 0...blockCount-1 {
                 print(n)
                 blockArray[n].physicsBody?.affectedByGravity = true
             }
         }
         currentBlock = nil
+        
+        
     }
     
     func newBlock(touchSpot: UITouch, theThing: String) {
@@ -232,10 +269,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         } else if blockThree.frame.contains(touch.previousLocation(in: self)) {
             newBlock(touchSpot: touch, theThing: "rock_L_horizontal")
         } else {
-            for n in 0...blockCount-1 {
-                if blockArray[n].frame.contains(touch.previousLocation(in: self)) {
-                    currentBlock = blockArray[n]
-                    break
+            
+            if blockCount >= 1 {
+                for n in 0...blockCount-1 {
+                    if blockArray[n].frame.contains(touch.previousLocation(in: self)) {
+                        currentBlock = blockArray[n]
+                        break
+                    }
                 }
             }
             
